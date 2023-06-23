7205b3630cd8c36f12ef8bf8f39d525f201e2087
Deepak Gohil <gohildeepak2003@gmail.com>
2023-06-23 22:31:35 +0530

    some console.logs added for debugging

--- utils/roast.js
+++ utils/roast.js
@@ -17,8 +17,10 @@ const roast = async (message) => {
 	})
 
 	let headers = { "Content-Type": "application/json" }
-
+	console.log(process.env.API_URL)
+	console.log(raw)
 	let roast_response = await fetch(process.env.API_URL, { method: 'POST', headers: headers, body: raw })
+	console.log(JSON.stringify(roast_response))
 	roast_response = await roast_response.json()
 	return roast_response
 }
